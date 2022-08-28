1d410fc0a27961cdb9af3a106477e5a40d5a32c9
iilj <u.eeoou@gmail.com>
2022-08-28 21:40:32 +0900

    Fix test

--- src/RailwayGraph.test.ts
+++ src/RailwayGraph.test.ts
@@ -5,13 +5,22 @@ describe('RailwayGraph', (): void => {
     test('should work.', async (): Promise<void> => {
         const jsonString = await fs.readFile('static/index.json', 'utf-8');
         const railwayGraph = RailwayGraph.initFromString(jsonString);
-        // TODO
-        expect(railwayGraph.getStationIdxByName('品川')).toBe(390);
-        expect(railwayGraph.getStationIdxByName('上野')).toBe(421);
-        // expect(railwayGraph.getShortestPathOfIdxs(390, 421)).toEqual([
-        //     390, 389, 388, 387,
-        //     386,  92, 309, 266,
-        //     420, 421
-        //   ]);
+
+        expect(railwayGraph.getShortestPathOfIdxs(
+            railwayGraph.getStationIdxByName('品川'),
+            railwayGraph.getStationIdxByName('上野')
+        )).toEqual([
+            railwayGraph.getStationIdxByName('品川'),
+            railwayGraph.getStationIdxByName('高輪ゲートウェイ'),
+            railwayGraph.getStationIdxByName('田町'),
+            railwayGraph.getStationIdxByName('浜松町'),
+            railwayGraph.getStationIdxByName('新橋'),
+            railwayGraph.getStationIdxByName('有楽町'),
+            railwayGraph.getStationIdxByName('東京'),
+            railwayGraph.getStationIdxByName('神田'),
+            railwayGraph.getStationIdxByName('秋葉原'),
+            railwayGraph.getStationIdxByName('御徒町'),
+            railwayGraph.getStationIdxByName('上野'),
+          ]);
     });
 });
